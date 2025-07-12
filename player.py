9440849067d88647f386ac3b70f6aed79a83395b
Loremgreg <gregorybastin.de@gmail.com>
2025-07-12 19:30:53 +0200

    Moving for&backward

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame 
 
 # La class Player est le plan pour créer un vaisseaux spatial. Elle décrit ce qu'un vaisseau est et ce qu'il peut faire.
@@ -22,6 +22,10 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def move(self, dt):
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
+            self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -30,5 +34,9 @@ class Player(CircleShape):
             self.rotate(-dt)            # dt négatif = rotation à gauche
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)            
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
-        
+   
